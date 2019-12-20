95945fc0ff99fd4293e5bdca7c3dae9c51de6dcc
= <=>
2019-12-20 00:59:41 +0000

    [Feature]: Update FileStorage: (models/engine/file_storage.py)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,19 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls:
+            objs = {}
+            for key, val in self.__objects.items():
+                if type(val).__name__ == cls.__class__.__name__:
+                    objs[key] = val
+            return objs
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +62,10 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete an object from __objects"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
+            self.save()
