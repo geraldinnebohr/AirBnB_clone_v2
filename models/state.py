629c2a5d383fb049cb1f8a45f56bf72b626a9ad1
= <=>
2019-12-20 01:09:59 +0000

    [Feature]: adding all files

--- models/state.py
+++ models/state.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref='state')
+
+    @property
+    def cities(self):
+        """method that returns the cities"""
+
+        otro_dict = models.storage.all(City)
+        otra_lista = []
+        for cositas, otras_cositas in otro_dict.items():
+            if otras_cositas.state_id == self.id:
+                otra_lista.append(otras_cositas)
+        return otra_lista
