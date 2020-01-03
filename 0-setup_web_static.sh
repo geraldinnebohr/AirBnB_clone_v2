dd54bd3b8dda6d8178988d2f978622bfd38fb591
geraldinnebohr <geraldinnebohr@gmail.com>
2020-01-03 20:28:40 +0000

    [FEATURE] Bash script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#Bash script that sets up the web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo service nginx start
+sudo mkdir -p /data/ /data/web_static/{shared,releases}/test/ && echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "s/server_name _;/server_name _;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}/" /etc/nginx/sites-available/default
+sudo service nginx restart
