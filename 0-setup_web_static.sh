585fddcb596b4939441a188f86cac76ad5c9e0e4
geraldinnebohr <geraldinnebohr@gmail.com>
2020-01-03 20:43:19 +0000

    [FIX] changed -R flag in chown

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,6 +11,6 @@ sudo mkdir -p /data/ /data/web_static/{shared,releases}/test/ && echo '<html>
   </body>
 </html>' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown ubuntu:ubuntu /data/ *
 sudo sed -i "s/server_name _;/server_name _;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}/" /etc/nginx/sites-available/default
 sudo service nginx restart
