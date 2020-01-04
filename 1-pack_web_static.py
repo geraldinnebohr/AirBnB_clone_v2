a3a7661631c9720639f6f922bf4676b0ba5e183d
geraldinnebohr <geraldinnebohr@gmail.com>
2020-01-04 03:21:23 +0000

    [FIX] pep8 style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Fabric script that generates a .tgz archive from the contents of the web_static folder
+"""Fabric script that generates a .tgz archive"""
 from fabric.api import local
 from datetime import datetime
 
